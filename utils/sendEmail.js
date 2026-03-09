384d5797f8429d9ca928bae6392e8abed3597503
Radnus Developer <webdevelopmentdept@gmail.com>
2026-03-09 17:41:25 +0530

    add new app password

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,13 +4,15 @@ const sendEmail = async (to, subject, text, pdfBuffer, fileName) => {
 
   try {
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
     const mailOptions = {
       from: `"RADNUS" <${process.env.EMAIL_USER}>`,
