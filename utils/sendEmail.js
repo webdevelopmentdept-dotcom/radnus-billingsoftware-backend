7bad77ca4a4219e6c21d0b6eab8111a701c10b6e
Radnus Developer <webdevelopmentdept@gmail.com>
2026-03-11 11:55:19 +0530

    Update code of sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,29 +1,68 @@
-const { Resend } = require('resend');
+// const { Resend } = require('resend');
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// const sendEmail = async (to, subject, text, pdfBuffer, fileName) => {
+//   console.log('--- pdf buffer ---',pdfBuffer)
+//   try {
+
+//     await resend.emails.send({
+//     from: 'RADNUS <noreply@service.radnus.in>',
+//       to: [to],
+//       subject: subject,
+//       text: text,
+//       attachments: [
+//         {
+//           filename: fileName || "file.pdf",
+//           content: pdfBuffer,
+//         },
+//       ],
+//     });
+
+//     console.log("Email sent successfully ✅");
+
+//   } catch (error) {
+//     console.error("EMAIL ERROR:", error);
+//     throw error;
+//   }
+// };
+
+// module.exports = sendEmail;
+
+const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text, pdfBuffer, fileName) => {
+
   try {
 
+    const attachments = pdfBuffer
+      ? [
+          {
+            filename: fileName || "file.pdf",
+            content: pdfBuffer.toString("base64"), // important fix
+          },
+        ]
+      : [];
+
     await resend.emails.send({
-    from: 'RADNUS <noreply@service.radnus.in>',
+      from: "RADNUS <noreply@service.radnus.in>",
       to: [to],
       subject: subject,
       text: text,
-      attachments: [
-        {
-          filename: fileName || "file.pdf",
-          content: pdfBuffer,
-        },
-      ],
+      attachments: attachments,
     });
 
     console.log("Email sent successfully ✅");
 
   } catch (error) {
+
     console.error("EMAIL ERROR:", error);
     throw error;
+
   }
+
 };
 
 module.exports = sendEmail;
